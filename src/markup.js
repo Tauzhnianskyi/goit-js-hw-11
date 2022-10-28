99f7c2df8ccb57f3caf60d2ded58c436fd567823
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-10-28 15:15:15 +0300

    Update markup.js

--- src/markup.js
+++ src/markup.js
@@ -1,41 +1,41 @@
-// export default function markup(img) {
-//   return img
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => {
-//         return `<div class="card">
-//         <div class="card__link"><a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width='370'/></a></div>
-//         <div class="card__info">
-//         <div class="card__link-item">
-//         <p>
-//         <b>Likes: </b><br>${likes}
-//         </p>
-//         </div>
-//         <div class="card__link-item">
-//         <p>
-//         <b>Views: </b><br>${views}
-//         </p>
-//         </div>
-//         <div class="card__link-item">
-//         <p>
-//         <b>Comments: </b><br>${comments}
-//         </p>
-//         </div>
-//         <div class="card__link-item">
-//         <p>
-//         <b>Downloads: </b><br>${downloads}
-//         </p>
-//         </div>
-//         </div>
-//         </div>`;
-//       }
-//     )
-//     .join('');
-// }
+export default function markup(img) {
+  return img
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="card">
+        <div class="card__link"><a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width='370'/></a></div>
+        <div class="card__info">
+        <div class="card__link-item">
+        <p>
+        <b>Likes: </b><br>${likes}
+        </p>
+        </div>
+        <div class="card__link-item">
+        <p>
+        <b>Views: </b><br>${views}
+        </p>
+        </div>
+        <div class="card__link-item">
+        <p>
+        <b>Comments: </b><br>${comments}
+        </p>
+        </div>
+        <div class="card__link-item">
+        <p>
+        <b>Downloads: </b><br>${downloads}
+        </p>
+        </div>
+        </div>
+        </div>`;
+      }
+    )
+    .join('');
+}
